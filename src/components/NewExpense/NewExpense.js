398880803f261a407605f4f71a6c59741f645a49
Robert Neal <rmn316@gmail.com>
2023-02-28 11:47:40 +0700

    Added event to parent app.js

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,13 +3,14 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm"
 import "./NewExpense.css"
 
-const NewExpense = () => {
+const NewExpense = (props) => {
 
     const saveExpenseHandler = (data) => {
         const expenseData = {
             ...data,
             id: Math.random().toString()
         }
+        props.onAddExpense(expenseData)
     }
 
     return (
